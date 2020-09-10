0252827da9b4c5caa130b97c7b9291d984f8a8e5
Bhuwan <Paneru>
2020-09-10 17:30:38 +0530

    project initiated

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoogleRecap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
